0623c7f46897b19e16ede08823f49d3380604c65
Pan-Marek <boteq6@o2.pl>
2017-12-06 13:25:56 +0100

    It works of course

--- day_3/Source.cpp
+++ day_3/Source.cpp
@@ -10,8 +10,6 @@ int main()
 	auto valid_part_one = aoc::count_valid_passwords(lines);
 	std::cout<< "Valid passphrases: " << valid_part_one << std::endl;
 
-
-	// TODO: doesn't work :/
 	auto valid_part_two = aoc::count_valid_passwords_part_two(lines);
 	std::cout << "Valid passphrases part two: " << valid_part_two << std::endl;
 
